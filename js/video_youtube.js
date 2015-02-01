b8e9736be58c876b8abb2c0c535c22147bab5074
Oguzhan Cansever <cansever@gjuce.com>
2015-02-01 21:48:03 +0100

    First commit

---
+++ js/video_youtube.js
@@ -0,0 +1,14 @@
+$('document').ready(function() {
+	"use strict";
+	if( !device.tablet() && !device.mobile() ) {
+		var options = { videoId: 'qkfGP4UTpjE', mute: false};
+		$('#video').tubular(options);
+} else {
+	
+	$('body').addClass('poster-image');
+	
+}
+
+});
+
+
